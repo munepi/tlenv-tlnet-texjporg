b419c92b6505f22f06a7b7e4e0ebf2cc99ed139c
Munehiro Yamamoto <munepixyz@gmail.com>
2022-12-12 20:19:15 +0900

    etc/tlenv.d/install/tlnet-texjporg.bash: updated

--- etc/tlenv.d/install/tlnet-texjporg.bash
+++ etc/tlenv.d/install/tlnet-texjporg.bash
@@ -2,24 +2,36 @@ before_install export_texlive_install_tlnet
 
 export_texlive_install_tlnet() {
     local tlnet_texjporg=http://texlive.texjp.org/
+
+    ## auto-detect TeX Live VERSION_NAME
+    local detected_version_name
     case "$VERSION_NAME" in
         # current)  ##=> proxy to JAIST or RIKEN
         #     export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME}/tlnet/
         #     ;;
         pretest)
-            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME}/tlnet/
+            detected_version_name=${VERSION_NAME}
             ;;
         [0-9][0-9][0-9][0-9])
-            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME}/tlnet/
+            detected_version_name=${VERSION_NAME}
             ;;
         [0-9][0-9][0-9][0-9]-*)
-            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME%-*}/tlnet/
+            detected_version_name=${VERSION_NAME%-*}
             ;;
         *-[0-9][0-9][0-9][0-9])
-            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME#*-}/tlnet/
+            detected_version_name=${VERSION_NAME#*-}
             ;;
         [0-9][0-9][0-9][0-9].*)
-            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${VERSION_NAME%.*}/tlnet/
+            detected_version_name=${VERSION_NAME%.*}
+            ;;
+    esac
+
+    ## set tlnet
+    case "${detected_version_name}" in
+        201[2-9]|20[2-9][0-9])  ##=> 2012--2099
+            export TEXLIVE_INSTALL_TLNET=${tlnet_texjporg}${detected_version_name}/tlnet/
+            ;;
+        *)
             ;;
     esac
 }
